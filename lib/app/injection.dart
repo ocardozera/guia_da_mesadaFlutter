2bdf81b52d5ef77c2570daefe7221a209dafc5b1
Matheus Cardoso <matheusc.controlenamao@gmail.com>
2021-07-07 20:59:33 -0300

    Gestão de estado

--- lib/app/injection.dart
+++ lib/app/injection.dart
@@ -1,13 +1,12 @@
 import 'package:get_it/get_it.dart';
 import 'package:guia_da_mesada/app/database/sqlite/dao/filho_dao_implementacao.dart';
 import 'package:guia_da_mesada/app/domain/entities/interfaces/filho_dao.dart';
+import 'package:guia_da_mesada/app/domain/services/filho_service.dart';
 
 
 setupInjection() {
   GetIt getIt = GetIt.I;
 
   getIt.registerSingleton<FilhoDAO>(FilhoDAOImplementacao());
-  
-
-
+  getIt.registerSingleton<FilhoService>(FilhoService());
 }
